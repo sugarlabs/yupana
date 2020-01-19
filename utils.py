41503dd7b2e1b35695cf48664386a41d9c91dd1c
Abhay-dot <55094435+Abhay-dot@users.noreply.github.com>
2020-01-19 12:25:05 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@
 # Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
 
 
-from StringIO import StringIO
+from io import StringIO
 import json
 json.dumps
 from json import load as jload
