7b7cdd4832745d8c68ec883c52ee27666bd8647e
Saumya-Mishra9129 <saumyamishra9129853131@gmail.com>
2020-06-27 00:01:31 +0530

    Add Gtk and Gdk version info

--- YupanaActivity.py
+++ YupanaActivity.py
@@ -9,7 +9,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this library; if not, write to the Free Software
 # Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
-
+import gi
+gi.require_version('Gdk','3.0')
+gi.require_version('Gtk','3.0')
 from gi.repository import Gtk, Gdk, GObject
 
 from sugar3.activity import activity
